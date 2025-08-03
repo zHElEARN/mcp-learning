1b666c4f30bea16221e7dd3d50bfcd74f4dbedab
zHElEARN <personal@zhelearn.com>
2025-08-03 13:38:59 +0800

    忽略构建时产生的警告

--- mcp-frontend/src/app/chat/[id]/page.tsx
+++ mcp-frontend/src/app/chat/[id]/page.tsx
@@ -188,6 +188,7 @@ export default function ConversationPage() {
 
     chatContainer.addEventListener("scroll", handleScroll);
     return () => chatContainer.removeEventListener("scroll", handleScroll);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [chatContainerRef.current]);
 
   return (
